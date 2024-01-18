1e6c489e86f1b00ffd1adf487a76ffccba327151
Alejandro Zarate <alejandrozaratecordoba@gmail.com>
2024-01-18 04:43:15 -0600

    Funcion lambda y suma

--- functions.py
+++ functions.py
@@ -1,4 +1,16 @@
+from tkinter import *
 from tkinter import messagebox
+
 
 def mensaje():
     messagebox.showinfo("Título del mensaje", "Este es un mensaje de ejemplo.")
+
+def suma(n1 , n2, lbl_resultado):
+    num1= float(n1.get())
+    num2= float(n2.get())
+    resultado= float(num1) + float(num2)
+    lbl_resultado.config(text=f"La suma es: {resultado}")
+
+    
+    
+    
--- main.py
+++ main.py
@@ -1,19 +1,35 @@
 from tkinter import *
-from functions import mensaje
+from functions import suma
 
 inicio= Tk()
 
 inicio.title("Veterinaria")
 inicio.iconbitmap("images\window-icon.ico")
-inicio.geometry("950x650")
+mycolor = '#E0E0E0'  # or use hex if you prefer 
+inicio.geometry("900x450")
 #root.config(bg="black")
 
-lbl = Label(inicio, text='Este es un label')
-lbl.pack()
+lbl_1 = Label(inicio, text='Primer numero')
+lbl_1.place(x=10, y=10, width=100, height=30)
 
-btn = Button(inicio, text='Mensaje', command=mensaje)
-btn.config(fg="black", bg="white")
-btn.pack()
+txt_1 = Entry(inicio, bg=mycolor)
+txt_1.place(x=150, y=10, width=100, height=30)
+
+lbl_2 = Label(inicio, text='Segundo numero')
+lbl_2.place(x=10, y=50, width=100, height=30)
+
+txt_2 = Entry(inicio, bg=mycolor)
+txt_2.place(x=150, y=50, width=100, height=30)
+
+lbl_3 = Label(inicio, text="resultado")
+lbl_3.place(x=95, y=90, width=100, height=30)
+
+btn = Button(inicio, text='Mensaje', command=lambda: suma(txt_1, txt_2, lbl_3))
+btn.place(x=270, y=25, width=100, height=30)
+#btn.config(fg="blue", bg="white")
+btn["fg"]="white"
+btn["bg"]="black"
+
 
 inicio.mainloop()
 
